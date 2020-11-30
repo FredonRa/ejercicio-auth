7c1fbb5fd06dfa43f58e640af6848eedf82f91c0
Federico Leiva <fedeleiva20@gmail.com>
2020-11-30 17:53:05 -0300

    Updates

---
+++ precache-manifest.e4fa1bc9b7af77b66505e5dda1c5d89b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86bde7cb20ca3e51770d4566d0597f10",
+    "url": "/ejercicio-auth/index.html"
+  },
+  {
+    "revision": "6d0f16c7f648707524d6",
+    "url": "/ejercicio-auth/static/css/main.71e415e0.chunk.css"
+  },
+  {
+    "revision": "7aea5afceb84d5f603ca",
+    "url": "/ejercicio-auth/static/js/2.832030c8.chunk.js"
+  },
+  {
+    "revision": "6d0f16c7f648707524d6",
+    "url": "/ejercicio-auth/static/js/main.85119ad6.chunk.js"
+  },
+  {
+    "revision": "d6c966d22c3a411d897b",
+    "url": "/ejercicio-auth/static/js/runtime~main.53b26e27.js"
+  }
+]);
